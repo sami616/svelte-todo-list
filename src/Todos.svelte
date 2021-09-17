cfe348a0d9a64a814bc1c8fef96ba2f522a6a771
Sami <samuel.resua@bp.com>
2021-09-17 09:27:57 +0100

    update

--- src/Todos.svelte
+++ src/Todos.svelte
@@ -23,6 +23,7 @@
     todos = [...todos, { id: Math.random(), title: newTodo, completed: false }]
   }
 
+  // Handle removing a todo title
   function remove(event: CustomEvent<{ todo: ITodo }>) {
     if (!todos) return
     todos = todos.filter(curr => event.detail.todo.id !== curr.id)
