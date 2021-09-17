0ce6f1c18e437184310c4fe09853a72785c99339
Sami <samuel.resua@bp.com>
2021-09-17 09:25:51 +0100

    custom events

--- src/Todo.svelte
+++ src/Todo.svelte
@@ -1,32 +1,61 @@
-<!-- Types -->
-
 <script lang="ts" context="module">
-    export type ITodo = {
-		title: string
-		completed: boolean
-	};
+  export type ITodo = {
+    id: number
+    title: string
+    completed: boolean
+  }
+  type DispatchActions = Record<'remove' | 'toggle' | 'edit', { todo: ITodo }>
 </script>
 
-<!-- Logic -->
-
 <script lang="ts">
-    export let todo: ITodo
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher<DispatchActions>()
+  export let todo: ITodo
 </script>
 
 <!-- Markup -->
 
-<li class:completed={todo.completed}>{todo.title} <input type="checkbox" bind:checked={todo.completed} /></li>
+<li class:completed={todo.completed}>
+  <div>
+    <input
+      type="checkbox"
+      on:change={() => dispatch('toggle', { todo })}
+      checked={todo.completed}
+    />
+    <span>{todo.title}</span>
+  </div>
+  <div>
+    <button on:click={() => dispatch('edit', { todo })}>Edit</button>
+    <button on:click={() => dispatch('remove', { todo })}>Remove</button>
+  </div>
+</li>
 
 <!-- Styles -->
-
 <style>
-    li {
-        padding: 1rem;
-    }
-    li.completed {
-       text-decoration: line-through;
-    }
-    li:not(:last-child){
-        border-bottom: 1px solid #efefef;
-    }
+  div {
+    display: inherit;
+    align-items: inherit;
+    gap: inherit;
+  }
+  li {
+    padding: 1rem 0;
+    display: flex;
+    gap: 1rem;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  input {
+    margin: 0;
+  }
+
+  li.completed span {
+    text-decoration: line-through;
+  }
+  li:not(:last-child) {
+    border-bottom: 1px solid #efefef;
+  }
+  button {
+    margin: 0;
+  }
 </style>
--- src/Todos.svelte
+++ src/Todos.svelte
@@ -1,41 +1,80 @@
 <!-- Types -->
-
 <script lang="ts" context="module">
-	type ITodos = ITodo[];
+  import type { ITodo } from './Todo.svelte'
 </script>
 
 <!-- Logic -->
-
 <script lang="ts">
-	import Todo from './Todo.svelte'
-	import type { ITodo } from './Todo.svelte'
+  import Todo from './Todo.svelte'
+  import { onMount } from 'svelte'
 
-	async function fetchTodos(): Promise<ITodo[]>{
-		const res = await fetch('https://jsonplaceholder.typicode.com/todos')
-		if (!res.ok) throw new Error()
-		return await res.json()
-	}
+  let todos: ITodo[] | undefined
+
+  // Handle getting the todos from localStorage
+  function getTodos() {
+    const stringTodos = localStorage.getItem('todos')
+    todos = stringTodos ? JSON.parse(stringTodos) : []
+  }
+
+  // Handle adding a todo
+  function add() {
+    const newTodo = prompt('Add a todo')
+    if (!newTodo || !todos) return
+    todos = [...todos, { id: Math.random(), title: newTodo, completed: false }]
+  }
+
+  function remove(event: CustomEvent<{ todo: ITodo }>) {
+    if (!todos) return
+    todos = todos.filter(curr => event.detail.todo.id !== curr.id)
+  }
+
+  // Handle toggling a todo's completed status
+  function toggle(event: CustomEvent<{ todo: ITodo }>) {
+    if (!todos) return
+    todos = todos.map(curr => {
+      if (curr.id === event.detail.todo.id) curr.completed = !curr.completed
+      return curr
+    })
+  }
+
+  // Handle editing a todo title
+  function edit(event: CustomEvent<{ todo: ITodo }>) {
+    const updated = prompt('Edit todo', event.detail.todo.title)
+    if (!updated || !todos) return
+    todos = todos.map(curr => {
+      if (curr.id === event.detail.todo.id) curr.title = updated
+      return curr
+    })
+  }
+
+  // Update localStorage whenever the `todos` value changes
+  $: {
+    if (todos) localStorage.setItem('todos', JSON.stringify(todos))
+  }
+
+  // Get the todos on mount
+  onMount(getTodos)
 </script>
 
 <!-- Markup -->
 
-{#await fetchTodos()}
-	<p>Loading</p>
-{:then todos}
-	<ul>
-		{#each todos as todo}
-			<Todo {todo}/>
-		{/each}
-	</ul>
-{:catch error}
-	<p>Error</p>
-{/await}
+{#if todos}
+  <button on:click={add}>Add todo</button>
+  {#if todos.length === 0}
+    <p>No todos</p>
+  {:else}
+    <ul>
+      {#each todos as todo (todo.id)}
+        <Todo on:remove={remove} on:edit={edit} on:toggle={toggle} {todo} />
+      {/each}
+    </ul>
+  {/if}
+{/if}
 
 <!-- Styles -->
-
 <style>
-	ul {
-		padding: 0;
-		margin: 0;
-	}
+  ul {
+    padding: 0;
+    margin: 0;
+  }
 </style>
